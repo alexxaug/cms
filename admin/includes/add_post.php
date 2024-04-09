5a638f1a472d37e25b8953d85bacda43d3cd4a78
alexxaug <alexxaug@outlook.com>
2024-04-09 06:25:21 +0200

    remove post author from POSTing of the data for post, readded using the session where we already get the user

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -3,7 +3,7 @@
         $post_title = $_POST['title'];
         $post_category_id = $_POST['post_category_id'];
         $post_status = $_POST['post_status'];
-
+        $post_author = $_SESSION['username'];
         $post_image = $_FILES['image']['name'];
         $post_image_temp = $_FILES['image']['tmp_name'];
 
@@ -14,8 +14,8 @@
 
         move_uploaded_file($post_image_temp, "../images/$post_image");
 
-        $query = "INSERT INTO posts(post_category_id, post_title, post_date, post_image, post_content, post_tags, post_status) ";
-        $query .= "VALUES({$post_category_id}, '{$post_title}', now(), '{$post_image}', '{$post_content}',
+        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
+        $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}',
         '{$post_tags}', '{$post_status}') ";
 
         $create_post_query = mysqli_query($connection, $query);
