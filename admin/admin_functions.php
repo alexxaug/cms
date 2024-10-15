777da703a39929457950ede36634296798338113
alexxaug <alexxaug@outlook.com>
2024-10-15 06:44:48 +0200

    Registration page completed, error arrays to detect and display errors (password empty, email or username exists etc)

--- admin/admin_functions.php
+++ admin/admin_functions.php
@@ -156,6 +156,60 @@
       };
     };
 
+    function registerUser($username, $email, $password, $firstname, $lastname){
+      global $connection;
+
+          $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+
+          $query = "INSERT INTO users(username, user_password, user_email, user_role, user_firstname, user_lastname) ";
+          $query.= "VALUES('{$username}', '{$password}', '{$email}', 'subscriber', '{$firstname}', '{$lastname}') ";
+
+          $register_user_query = mysqli_query($connection, $query);
+
+          confirm_query($register_user_query);
+
+          echo "<p class='text-center bg-success'> User registered. </p>";
+
+    };
+
+    function loginUser($username, $password){
+      global $connection;
+      $username = trim($username);
+      $password = trim($password);
+
+      $username = mysqli_real_escape_string($connection, $username);
+      $password = mysqli_real_escape_string($connection, $password);
+
+      $query = "SELECT * FROM users WHERE username = '{$username}' ";
+      $select_user_query = mysqli_query($connection, $query);
+      if(!$select_user_query){
+        die("Query Failed" . mysqli_error($connection));
+      };
+
+      while($row = mysqli_fetch_array($select_user_query)){
+        $db_id = $row['user_id'];
+        $db_username = $row['username'];
+        $db_user_password = $row['user_password'];
+        $db_user_firstname = $row['user_firstname'];
+        $db_user_lastname = $row['user_lastname'];
+        $db_user_role = $row['user_role'];
+        $db_user_email = $row['user_email'];
+      };
+
+        if(password_verify($password,$db_user_password)){
+          $_SESSION['username'] = $db_username;
+          $_SESSION['firstname'] = $db_user_firstname;
+          $_SESSION['lastname'] = $db_user_lastname;
+          $_SESSION['user_role'] = $db_user_role;
+          $_SESSION['user_email'] = $db_user_email;
+
+          header("Location: /cms/admin");
+        } else {
+          header("Location: /cms/");
+        };
+    };
+
+
     function users_online(){
 
       if(isset($_GET['onlineusers'])){
--- registration.php
+++ registration.php
@@ -8,40 +8,83 @@
  ?>
 
 <?php
-  if(isset($_POST['submit'])){
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
 
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
+    $username = trim($_POST['username']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $firstname = trim($_POST['firstname']);
+    $lastname = trim($_POST['lastname']);
 
-    if(!empty($username) && !empty($password) && !empty($email)){
+    //checking for error in form and storing that message
+    $error = [
+      'username'=> '',
+      'email'=> '',
+      'password'=> ''
+    ];
 
-      if(!usernameExists($username) && !emailExists($email)){
-
-      $username = mysqli_real_escape_string($connection, $username);
-      $email = mysqli_real_escape_string($connection, $email);
-      $password = mysqli_real_escape_string($connection, $password);
-
-      $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
-
-      $query = "INSERT INTO users(username, user_password, user_email, user_role, user_firstname, user_lastname) ";
-      $query.= "VALUES('{$username}', '{$password}', '{$email}', 'subscriber', '{$firstname}', '{$lastname}') ";
-
-      $register_user_query = mysqli_query($connection, $query);
-
-      confirm_query($register_user_query);
-
-      echo "<p class='text-center bg-success'> User registered. </p>";
-      echo "<script>setTimeout(\"location.href = 'index.php';\",1500);</script>";
-    } else {
-      echo "<p class='text-center bg-warning'>That username and/ or email exists. Please choose an alternative or login.</p>";
+    if(strlen($username) < 4){
+      $error['username'] = 'Username needs atleast 4 characters.';
     };
-    } else {
-      echo "<p class='text-center bg-warning'> Fields ('username', 'email' and 'password  ') cannot be empty. </p>";
-      echo "<script>setTimeout(\"location.href = 'registration.php';\",2000);</script>";
+
+    if($username == ''){
+      $error['username'] = 'Username cannot be empty.';
     };
+
+    if($email == ''){
+      $error['email'] = 'Email cannot be empty.';
+    };
+
+    if($password == ''){
+      $error['password'] = 'Password cannot be empty.';
+    };
+
+    if(usernameExists($username)){
+      $error['username'] = 'This username is taken, please pick another one.';
+    };
+
+    if(emailExists($email)){
+      $error['email'] = 'An existing account is linked to this email. Try <a href="index.php">logging in.</a>';
+    };
+
+    foreach($error as $key => $value){
+      if(empty($value)){
+         unset($error[$key]);
+      };
+    }; // for each end
+
+    if(empty($error)){
+      registerUser($username, $email, $password, $firstname, $lastname);
+      loginUser($username, $password);
+    };
+
+
+    // if(!empty($username) && !empty($password) && !empty($email)){
+    //
+    //   if(!usernameExists($username) && !emailExists($email)){
+    //
+    //   $username = mysqli_real_escape_string($connection, $username);
+    //   $email = mysqli_real_escape_string($connection, $email);
+    //   $password = mysqli_real_escape_string($connection, $password);
+    //
+    //   $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+    //
+    //   $query = "INSERT INTO users(username, user_password, user_email, user_role, user_firstname, user_lastname) ";
+    //   $query.= "VALUES('{$username}', '{$password}', '{$email}', 'subscriber', '{$firstname}', '{$lastname}') ";
+    //
+    //   $register_user_query = mysqli_query($connection, $query);
+    //
+    //   confirm_query($register_user_query);
+    //
+    //   echo "<p class='text-center bg-success'> User registered. </p>";
+    //   echo "<script>setTimeout(\"location.href = 'index.php';\",1500);</script>";
+    // } else {
+    //   echo "<p class='text-center bg-warning'>That username and/ or email exists. Please choose an alternative or login.</p>";
+    // };
+    // } else {
+    //   echo "<p class='text-center bg-warning'> Fields ('username', 'email' and 'password  ') cannot be empty. </p>";
+    //   echo "<script>setTimeout(\"location.href = 'registration.php';\",2000);</script>";
+    // };
 
   };
 
@@ -64,26 +107,37 @@
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                         <div class="form-group">
                             <label for="username" class="sr-only">Username</label>
-                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
+                            <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username" autocomplete="on"
+                            value="<?php echo isset($username) ? $username : ''?>">
+                            <!-- displaying errors if any -->
+                            <p><?php echo isset($error['username']) ? $error['username'] : ''?></p>
+
                         </div>
                         <div class="form-group">
                             <label for="firstname" class="sr-only">Firstname</label>
-                            <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Enter Firstname - optional">
+                            <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Enter Firstname - optional" autocomplete="on"
+                            value="<?php echo isset($firstname) ? $firstname : ''?>">
                         </div>
                         <div class="form-group">
                             <label for="lastname" class="sr-only">Lastname</label>
-                            <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Enter Lastname - optional">
+                            <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Enter Lastname - optional" autocomplete="on"
+                            value="<?php echo isset($lastname) ? $lastname : ''?>">
                         </div>
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                            <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com" autocomplete="on"
+                            value="<?php echo isset($email) ? $email : ''?>">
+                            <!-- displaying errors if any -->
+                            <p><?php echo isset($error['email']) ? $error['email'] : ''?></p>
                         </div>
                          <div class="form-group">
                             <label for="password" class="sr-only">Password</label>
                             <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                            <!-- displaying errors if any -->
+                            <p><?php echo isset($error['password']) ? $error['password'] : ''?></p>
                         </div>
 
-                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                        <input type="submit" name="register" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
                     </form>
 
                 </div>
